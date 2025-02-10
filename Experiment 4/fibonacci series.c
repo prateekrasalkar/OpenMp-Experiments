26703b3ed040859613cf5ae69b2f35cb6b2d7742
Prateek Rasalkar <prasalkar916@gmail.com>
2025-02-10 17:02:23 +0530

    Update fibonacci series.c

--- Experiment 4/fibonacci series.c
+++ Experiment 4/fibonacci series.c
@@ -1,63 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
+#include<stdio.h>
+#include<omp.h>
+int main(){
+int a=1,b=0,c=0;
+int i=0,n=0;
+printf ("Enter n for fibonisis series ");
+scanf("%d",&n);
+int arr[n];
+double start=omp_get_wtime();
+#pragma omp parallel for 
+for(i=0;i<n;i++)
+{
 
-// Fibonacci function using recursion with memoization
-int fib(int a, int *b) {
-    if (a <= 1)
-        return a;
-    
-    if (b[a] != -1)  // Return if already computed
-        return b[a];
-
-    int x, y;
-
-    #pragma omp task shared(b) firstprivate(a)
-    x = fib(a - 1, b);
-
-    #pragma omp task shared(b) firstprivate(a)
-    y = fib(a - 2, b);
-
-    #pragma omp taskwait  // Ensure x and y are computed before storing the result
-    b[a] = x + y;
-    return b[a];
+ 
+  c=a+b;
+  arr[i]=c;
+  a=b;
+  b=c;
+  
 }
+double end =omp_get_wtime();
+printf("\n Time=%f\n",end-start);
+for(i=0;i<n;i++){
+printf(" %d",arr[i]);
+}
+return 0;
 
-int main() {
-    int a;
-    
-    // Taking user input
-    printf("Enter the number of Fibonacci terms to print: ");
-    scanf("%d", &a);
-    
-    if (a < 0) {
-        printf("Please enter a non-negative integer.\n");
-        return 1;
-    }
 
-    int *b = (int *)malloc((a + 1) * sizeof(int));
-
-    // Initialize memoization array with -1
-    for (int c = 0; c <= a; c++)
-        b[c] = -1;
-
-    #pragma omp parallel
-    {
-        #pragma omp single  // Ensures only one thread starts the computation
-        {
-            for (int c = 0; c < a; c++) {
-                fib(c, b);
-            }
-        }
-    }
-
-    // Print Fibonacci series
-    printf("Fibonacci Series up to %d terms:\n", a);
-    for (int c = 0; c < a; c++) {
-        printf("%d ", b[c]);
-    }
-    printf("\n");
-
-    free(b); // Free allocated memory
-    return 0;
 }
