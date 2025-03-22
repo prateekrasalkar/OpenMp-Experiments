04174a82ac1777c9fb280b93fd47d89e607787a1
Prateek Rasalkar <prasalkar916@gmail.com>
2025-03-22 12:51:51 +0530

    Update listranking.c

--- Experiment 6/listranking.c
+++ Experiment 6/listranking.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
-#include <omp.h>
-
-typedef struct { int next, rank; } Node;
-
-void parallel_list_ranking(Node* list, int N) {
-    int changed = 1;
-    while (changed) {
-        changed = 0;
-        #pragma omp parallel for
-        for (int i = 0; i < N; i++)
-            if (list[i].next != -1) {
-                list[i].rank += list[list[i].next].rank;
-                list[i].next = list[list[i].next].next;
-                if (list[i].next != -1) changed = 1;
-            }
-    }
-}
+#include<omp.h>
 
 int main() {
-    Node list[] = {{1,1}, {2,1}, {3,1}, {4,1}, {-1,1}}; // Example: A->B->C->D->E
-    int N = 5;
 
-    parallel_list_ranking(list, N);
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n=6;
+     int i, j, temp;
+    #pragma omp parallel for
+    for (i = n ;i > 0;i--) {
+    #pragma omp parallel for
+        for (j = i-1; j > 0; j--) {
+            if (arr[i] < arr[j ]) {
+                
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j ] = temp;
+            }
+        }
+    }
+    int p=1;
+     for (int i = n-1; i >=0; i--) {
+        printf("Element = %d Rank = %d \n", arr[i],p);
+        p++;
+    }
+    printf("\n");
+  
 
-    for (int i = 0; i < N; i++)
-        printf("Node %d: Rank = %d\n", i, list[i].rank);
     return 0;
 }
